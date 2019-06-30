001f026bd9f7956d02c0890ffaf79d1126898b22
Alex Tang <cpl.alexandtang@gmail.com>
2019-06-30 14:42:15 +0800

    Remove sensitive info

--- Final/backend/config.py
+++ Final/backend/config.py
@@ -1,8 +1,8 @@
 # CONFIGs of chatbot
 
 lineConfig = {
-	"token" : "U+5yEccWvyBN1lENmNigc2TexAujZV+ofhGh2CLhNauFJaNgOreN3sTz6fh5E4NdvFGxfxhZb55mtN1e3bHllwd6osRScOIMl7WG1bvPqrvARynZUyl+eC9avtQq0XDZRN6d+2PJ6PkQROkjcMlBhgdB04t89/1O/w1cDnyilFU=",
-	"secret" : "a54f6fd0a39c82fc235ead25325fdbb2"
+	"token" : "",
+	"secret" : ""
 }
 
 messengerConfig = {
@@ -11,5 +11,5 @@ messengerConfig = {
 }
 
 googleConfig = {
-	"token" : "AIzaSyAH6neeTIGmbC8rLxcXHa2rPlabZ-BdWGA"
+	"token" : ""
 }
